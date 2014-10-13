ed9c9ae4d611c080a0d71568af2acc0db66f73e3
jambonbill <jambonbill@gmail.com>
2014-10-13 13:19:26 +0800

    new file

---
+++ 800px.php
@@ -0,0 +1,8 @@
+<table width='800px' border='1'>
+<tr><td style='padding:10px'>800px</td></tr>
+</table>
+
+<div class="row">
+	<div class="col-sm-5"><pre>col-sm-4</pre></div>
+	<div class="col-sm-7"><pre>col-sm-8</pre></div>
+</div>
---
+++ part3.php
@@ -0,0 +1,19 @@
+
+<h2>Part 3 - ?</h2>
+
+
+<div class="row">
+
+	<div class="col-sm-4">
+		<pre>col-sm-4</pre>
+	</div>
+	
+	<div class="col-sm-8">
+ 		<pre>col-sm-8</pre>
+ 	</div>
+
+</div>
+
+
+<script>
+</script>
