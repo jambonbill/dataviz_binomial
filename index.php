ff67fd9cd175a097048a2bab881a0ddb96556215
jambonbill <jambonbill@gmail.com>
2014-10-31 10:52:51 +0800

    Charset utf-8

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>Binomial distribution</title>
 
